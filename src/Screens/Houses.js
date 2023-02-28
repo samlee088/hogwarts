394c8ece98668f15b9a94074421c51e255018b1e
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-02-27 20:16:33 -0800

    able to render houses for selection

--- src/Screens/Houses.js
+++ src/Screens/Houses.js
@@ -1,5 +1,8 @@
 import React , {useState, useEffect} from 'react';
 import axios from '../axios';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import './Houses.css';
 
 const Houses = ({fetchURL}) => {
 
@@ -7,12 +10,10 @@ const Houses = ({fetchURL}) => {
 
     useEffect(()=> {
         async function fetchData() {
-            console.log(fetchURL);
             const request = await axios.get(fetchURL);
-            setHouses(request.data.results);
-            console.log(request);
+            setHouses(request.data);
+
             console.log(request.data);
-            console.log(request.data.results);
 
             return request;
         }
@@ -23,7 +24,29 @@ const Houses = ({fetchURL}) => {
         <div>
             <h1> Choose your House </h1>
 
-            
+            <div className='houses_houseCards'>
+                {houses.map((house) => (
+                    <Card style={{ width: '18rem' }} key={house.name}>
+                    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                    <Card.Body>
+                        <Card.Title>{house.name}</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            {house.traits.map((trait) => (
+                                <ListGroup.Item key={trait.name}>{trait.name}</ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    <Card.Body>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                    </Card>
+                ))}
+            </div>
 
         </div>
     )
